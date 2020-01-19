1dd933c475fe2a8d67d990b4234edf993891cac9
asifbinzia <asifbinzia@gmail.com>
2020-01-18 17:59:25 -0700

    Search for the index of a number in roated sorted array in logn time.

---
+++ SearchInRoatetedSortedArray.py
@@ -0,0 +1,53 @@
+class Solution:
+    def orderedSearch(self, nums: List[int], l, r, target: int):
+        
+        if l<= r:
+            mid = l + (r - l)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return self.orderedSearch(nums, mid+1, r, target)
+            else:
+                return self.orderedSearch(nums, l, mid-1, target)
+        else:
+            return -1
+
+
+
+
+
+    def unorderedSearch(self, nums: List[int], l, r, target: int):
+
+        if l<= r:
+            mid = l + (r-l)//2
+
+            if nums[mid] == target:
+                return mid
+            elif target > nums[mid]:
+                if nums[mid] > nums[r]:
+                    return self.unorderedSearch(nums, mid +1,r, target)
+                else:
+                    if target<= nums[r]:
+                        return self.orderedSearch(nums, mid +1,r, target)
+                    else:
+                        return self.unorderedSearch(nums, l, mid-1, target)
+            else:
+                if nums[mid] < nums[l]:
+                    return self.unorderedSearch(nums, l, mid-1, target)
+                else:
+                    if target>= nums[l]:
+                        return self.orderedSearch(nums, l, mid-1, target)
+                    else:
+                        return self.unorderedSearch(nums, mid+1, r, target)
+        else:
+            return -1
+
+
+    def search(self, nums: List[int], target: int) -> int:
+        return self.unorderedSearch(nums, 0, len(nums) -1, target)
+
+nums =[4,5,6,7,0,1,2]
+test = Solution()
+print(test.search(nums, 0))
+
